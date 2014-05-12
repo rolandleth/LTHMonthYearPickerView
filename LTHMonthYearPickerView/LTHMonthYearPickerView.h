e1ad13b333a45eaa44649be93ea0dad12489c511
rolandleth <roland@rolandleth.com>
2014-05-12 19:50:18 +0200

    Improved the docs.

--- LTHMonthYearPickerView/LTHMonthYearPickerView.h
+++ LTHMonthYearPickerView/LTHMonthYearPickerView.h
@@ -15,9 +15,8 @@
 - (void)pickerDidPressDoneWithMonth:(NSString *)month andYear:(NSString *)year;
 - (void)pickerDidPressCancel;
 /**
- *  If you want to change your text field (and/or variables) dynamically by implementing any of the pickerDidSelect__ delegate methods, instead of doing the change when Done was pressed, you should implement this method too, so the Cancel button restores old values.
- *
- *  @param initialValues comes in the form of @{ "month" : month, @"year" : year }
+ @brief				  If you want to change your text field (and/or variables) dynamically by implementing any of the pickerDidSelect__ delegate methods, instead of doing the change when Done was pressed, you should implement this method too, so the Cancel button restores old values.
+ @param initialValues @{ "month" : month, @"year" : year }
  */
 - (void)pickerDidPressCancelWithInitialValues:(NSDictionary *)initialValues;
 @end
@@ -31,14 +30,12 @@
 
 
 /**
- *  Month / Year picker view, for those pesky Credit Card expiration dates and alike.
- *
- *  @param date           set to a date if you want the picker to be initialized with a specific month and year, otherwise it is initialized with the current month and year.
- *  @param shortMonths    set to YES if you want months to be returned as Jan, Feb, etc, set to NO if you want months to be returned as January, February, etc.
- *  @param numberedMonths set to YES if you want months to be returned as 01, 02, etc. This takes precedence over shortMonths if set to YES.
- *  @param showToolbar    set to YES if you want the picker to have a Cancel/Done toolbar.
- *
- *  @return a container view which contains the UIPicker and toolbar
+ @brief				   Month / Year picker view, for those pesky Credit Card expiration dates and alike.
+ @param date           set if you want the picker to be initialized with a specific month and year, otherwise it will be initialized with the current month and year.
+ @param shortMonths    set to YES if you want months to be returned as Jan, Feb, etc, set to NO if you want months to be returned as January, February, etc.
+ @param numberedMonths set to YES if you want months to be returned as 01, 02, etc. This takes precedence over shortMonths if set to YES.
+ @param showToolbar    set to YES if you want the picker to have a Cancel/Done toolbar.
+ @return a container view which contains the UIPicker and toolbar
  */
 - (id)initWithDate:(NSDate *)date shortMonths:(BOOL)shortMonths numberedMonths:(BOOL)numberedMonths andToolbar:(BOOL)showToolbar;
 
